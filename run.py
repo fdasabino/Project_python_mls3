4dca7ce830e081d0b18945c5c3f9b33e472a5fbd
Francisco Sabino <fdasabino@gmail.com>
2021-06-17 07:48:01 +0200

    Create a separate function to handle drawing elements on the screen

--- run.py
+++ run.py
@@ -9,21 +9,33 @@ WIDTH, HEIGHT = 1050, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aliens Vs Nasa!")
 
+# colours for the window
 GRAY = (102, 102, 102)
+
+# frames per second
+FPS = 60
+
+
+# drawing function
+def drawing_elements():
+    WIN.fill(GRAY)
+    pygame.display.update()
+
 
 # main function
 def main():
     """
     Handles the main loop and the logic of the game.
     """
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        drawing_elements()
         
-        WIN.fill(GRAY)
-        pygame.display.update()
     pygame.quit()
 
 if __name__ == '__main__':
