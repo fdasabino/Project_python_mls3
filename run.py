a6eba075a796f8f80d2f63e41ef3e4f09357f136
Francisco Sabino <fdasabino@gmail.com>
2021-06-17 07:38:23 +0200

    Add main function to run the game and imported libraries

--- run.py
+++ run.py
@@ -1,1 +1,30 @@
 # python code goes here
+import pygame
+import os
+pygame.font.init()
+pygame.mixer.init()
+
+# Global variables
+WIDTH, HEIGHT = 1050, 900
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Aliens Vs Nasa!")
+
+GRAY = (102, 102, 102)
+
+# main function
+def main():
+    """
+    Handles the main loop and the logic of the game.
+    """
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        WIN.fill(GRAY)
+        pygame.display.update()
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
